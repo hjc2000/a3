46f9f0939c18a8525199aa75511c679f0a1f020e
hjc2000 <hjc.666@foxmail.com>
2023-10-09 21:47:02 +0800

    * 删除版权信息 * 删除 TRANSFORM_RF_TEST 宏定义

--- sample/app_stream/app_stream.cpp
+++ sample/app_stream/app_stream.cpp
@@ -1,37 +1,4 @@
-﻿//----------------------------------------------------------------------------
-//
-// Vision Advance Technology - Software Development Kit
-// Copyright (c) 2014-2022, Vision Advance Technology Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice,
-//    this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-// THE POSSIBILITY OF SUCH DAMAGE.
-//
-//----------------------------------------------------------------------------
-//
-//  VATek modulator sample code.
-//
-//----------------------------------------------------------------------------
-
-#include <vatek_sdk_usbstream.h>
+﻿#include <vatek_sdk_usbstream.h>
 #include <vatek_sdk_device.h>
 #include <cross/cross_os_api.h>
 
@@ -40,7 +7,6 @@
 #include "../common/inc/tool_stream.h"
 #include <core/tools/tool_dvb_t2.h>
 
-#define TRANSFORM_RF_TEST 0
 #define TRANSFORM_NORMAL 1
 
 static usbstream_param usbcmd;
@@ -127,11 +93,6 @@ int main(int argc, char *argv[])
 			- open usb_stream open
 			- config used usb_stream sync mode and start stream
 	*/
-
-	#if TRANSFORM_RF_TEST
-	vatek_device_start_test(hchip, (Pmodulator_param)&usbcmd.modulator, 900000);
-	vatek_device_start_sine(hchip, 900000);
-	#endif
 
 	#if TRANSFORM_NORMAL
 	if (is_vatek_success(nres))
