db7d3c10723852e48f68fb32f47708144bed0ff5
hjc2000 <huangjincanqqmail@qq.com>
2023-10-10 15:52:50 +0800

    删除cross_os_api.c

--- api/core/src/cross_os_api.cpp
+++ api/core/src/cross_os_api.cpp
@@ -19,3 +19,16 @@ void cross_os_yield()
 {
 	this_thread::yield();
 }
+
+#ifdef _MSC_VER
+#define PATH_BACKSLASH	'\\'
+#else
+#define PATH_BACKSLASH	'/'
+#endif
+
+void cross_os_append_path(char *path, const char *append)
+{
+	int32_t len = (int32_t)strlen(path);
+	if (path[len - 1] != PATH_BACKSLASH)path[len++] = PATH_BACKSLASH;
+	strcpy(&path[len], append);
+}
